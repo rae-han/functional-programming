4b07b6057f3a670219bd8fd1c884ca0136ef3711
fe-정래한(Raehan) <raehan.jeong@cj.net>
2025-05-01 21:48:53 +0900

    feature: filter 기본 구현

---
+++ src/basic/filter.js
@@ -0,0 +1,33 @@
+const products = [
+  { name: "반팔티", price: 15000 },
+  { name: "긴팔티", price: 20000 },
+  { name: "핸드폰케이스", price: 15000 },
+  { name: "후드티", price: 30000 },
+  { name: "바지", price: 25000 },
+];
+
+// const filter = () => {
+//   let result = [];
+
+//   for (const item of products) {
+//     if (item.price > 20000) {
+//       result.push(item);
+//     }
+//   }
+// };
+
+// filter();
+
+const filter = (fn, iter) => {
+  let result = [];
+
+  for (const item of iter) {
+    if (fn(item)) {
+      result.push(item);
+    }
+  }
+
+  return result;
+};
+
+console.log(filter((p) => p.price > 20000, products));
