9fa04f4f38ddf8a8d4d38663a84fd3223927f971
fe-정래한(Raehan) <raehan.jeong@cj.net>
2025-05-13 00:56:36 +0900

    feature: callback vs promise

--- src/asynchronous_concurrency/callback.js
+++ src/asynchronous_concurrency/callback.js
@@ -2,4 +2,10 @@ const add10 = (a, callback) => {
 	setTimeout(() => callback(a + 10), 100);
 };
 
-add10(1, (res) => console.log(res));
+add10(1, console.log);
+
+add10(1, (res) => {
+	add10(res, (res) => {
+		add10(res, console.log);
+	});
+});
--- src/asynchronous_concurrency/promise.js
+++ src/asynchronous_concurrency/promise.js
@@ -1,5 +1,23 @@
 const add10 = (a) => {
-	return new Promise((resolve) => setTimeout(() => resolve(a + 10), 1000));
+  return new Promise((resolve) =>
+    setTimeout(
+      () => resolve(a + 10),
+      100,
+    ),
+  );
 };
 
-add10(1).then((res) => console.log(res));
+const promise = add10(1).then(
+  console.log,
+);
+
+add10(1)
+  .then(add10)
+  .then(add10)
+  .then(console.log);
+
+const a = add10(1);
+const b = a.then((v) => v + 5);
+const c = b.then((v) => v + 5);
+const d = c.then((v) => v + 5);
+d.then(console.log);
