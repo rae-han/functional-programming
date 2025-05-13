a02e34a18a6e24bdb2375e85f9953b8decdd63a1
rae-han <raehan900809@gmail.com>
2025-05-13 09:07:24 +0900

    feature: 동기, 비동기 함수 똑같이 실행되게 수정

--- src/asynchronous_concurrency/promise2.js
+++ src/asynchronous_concurrency/promise2.js
@@ -1,4 +1,5 @@
-const go1 = (a, f) => f(a);
+// const go1 = (a, f) => f(a);
+const go1 = (a, f) => a instanceof Promise ? a.then(f) : f(a);
 const add5 = a => a + 5;
 
 console.log(go1(10, add5));
@@ -7,4 +8,10 @@ console.log(go1(Promise.resolve(10), add5)); // [object Promise]5
 
 const delay100 = a => new Promise(resolve => setTimeout(() => resolve(a), 100));
 
-console.log(go1(delay100(10), add5)); // [object Promise]15
+console.log(go1(delay100(10), add5)); // [object Promise]5
+go1(delay100(10), add5).then(console.log);
+
+go1(go1(10, add5),console.log);
+go1(go1(delay100(10), add5),console.log);
+
+
