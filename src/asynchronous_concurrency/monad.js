1aaf94f96bbda284d67fbf9f824317a44c13d147
rae-han <raehan900809@gmail.com>
2025-05-24 00:08:03 +0900

    feature: 실제 promise가 monad로 안전하게 함수를 합성하며 다뤄지는 예시

--- src/asynchronous_concurrency/monad.js
+++ src/asynchronous_concurrency/monad.js
@@ -8,3 +8,5 @@ Promise.resolve(1).then(g).then(f).then(r => console.log(r))
 
 Array.of().map(g).map(f).forEach(r => console.log(r)); // 출력 없음
 Promise.resolve().then(g).then(f).then(r => console.log(r)) // NaN
+
+new Promise(resolve => setTimeout(() => resolve(1), 100)).then(g).then(f).then(r => console.log(r)); // 4
