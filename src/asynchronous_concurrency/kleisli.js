693d05d3ec42398c87f6d3093a464dea108941cc
rae-han <raehan900809@gmail.com>
2025-05-24 14:13:49 +0900

    feature: 현실 세계의 프로그래밍 예시

---
+++ src/asynchronous_concurrency/kleisli.js
@@ -0,0 +1,27 @@
+import { find } from '../fxjs.js';
+
+const users = [
+  { id: 1, name: 'AA' },
+  { id: 2, name: 'BB' },
+  { id: 3, name: 'CC' },
+];
+
+const getUserById = (id) =>
+  find((user) => user.id === id, users);
+const getName = ({ name }) => name;
+
+const f = getName;
+const g = getUserById;
+
+const fg = (id) => f(g(id));
+
+console.log(fg(2) === fg(2)); // true
+
+const before = fg(2);
+
+users.pop();
+users.pop();
+
+const after = fg(2);
+
+console.log(before == after);
