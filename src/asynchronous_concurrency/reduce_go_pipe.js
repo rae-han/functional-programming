2f983648a5557e88e03bc3bf2682bb16bf54b18b
rae-han <raehan900809@gmail.com>
2025-05-24 23:40:36 +0900

    feature: reduce 함수 reject 처리

--- src/asynchronous_concurrency/reduce_go_pipe.js
+++ src/asynchronous_concurrency/reduce_go_pipe.js
@@ -8,3 +8,12 @@ go(
   (a) => a + 10000,
   console.log,
 );
+
+go(
+  Promise.resolve(1),
+  (a) => a + 10,
+  (a) => Promise.reject('error~~!!'),
+  (a) => a + 1000,
+  (a) => a + 10000,
+  console.log,
+).catch(console.log);
